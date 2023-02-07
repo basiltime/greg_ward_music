51458c02b0da6302e6024152e7fbbd244a1b119a
basiltime <krystalcanfield04@gmail.com>
2023-02-07 16:10:09 -0500

    Add titles to iframes

--- src/albums.js
+++ src/albums.js
@@ -4,16 +4,16 @@ export default function Albums() {
             <div>
                 <h1>Albums</h1>
                 <div className="album">
-                    <iframe src="https://open.spotify.com/embed/album/7DmpUa7XMOXLHIY1jSPJnD" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Distant Dynamics" src="https://open.spotify.com/embed/album/7DmpUa7XMOXLHIY1jSPJnD" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="album">
-                    <iframe src="https://open.spotify.com/embed/album/2XidNykeLjtu2iqntPGzPA" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Invasion" src="https://open.spotify.com/embed/album/2XidNykeLjtu2iqntPGzPA" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="album">
-                    <iframe src="https://open.spotify.com/embed/album/6iPo8g0I7c0lsiSCnKFkhb" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Pocket Full of Dreams" src="https://open.spotify.com/embed/album/6iPo8g0I7c0lsiSCnKFkhb" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="album">
-                    <iframe src="https://open.spotify.com/embed/album/6feTbSCIresTOhBhVYSSS9" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Passive Empire" src="https://open.spotify.com/embed/album/6feTbSCIresTOhBhVYSSS9" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
             </div>
         </main>
--- src/listen.js
+++ src/listen.js
@@ -4,19 +4,19 @@ export default function Listen() {
             <div>
                 <h1>Listen</h1>
                 <div className="track">
-                    <iframe src="https://open.spotify.com/embed/track/5n0DLNWEFj6dJnErLHK1Uh" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Jimmy's gone"src="https://open.spotify.com/embed/track/5n0DLNWEFj6dJnErLHK1Uh" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="track">
-                    <iframe src="https://open.spotify.com/embed/track/1pgNL0MM7ebUmFoOti5EkE" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="Shadows Emerge" src="https://open.spotify.com/embed/track/1pgNL0MM7ebUmFoOti5EkE" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="track">
-                    <iframe src="https://open.spotify.com/embed/track/3E6Dp7evDrMbtlQsGaHBiU" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="calming calamity" src="https://open.spotify.com/embed/track/3E6Dp7evDrMbtlQsGaHBiU" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="track">
-                    <iframe src="https://open.spotify.com/embed/track/3qJUbYzL9mWgHhxm8pUsRc" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe title="pulling strings" src="https://open.spotify.com/embed/track/3qJUbYzL9mWgHhxm8pUsRc" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div className="track">
-                    <iframe src="https://open.spotify.com/embed/track/45XCTYSF4TGNcndQOcfeXZ" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe> 
+                    <iframe title="Prelude (Beta Remixed)" src="https://open.spotify.com/embed/track/45XCTYSF4TGNcndQOcfeXZ" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe> 
                 </div>
             </div>
         </main>
